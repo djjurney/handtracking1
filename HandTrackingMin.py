454b01aa17cbda25d599e4675498a74b4a9088b8
David Jurney <djurney@cnm.edu>
2023-03-30 20:58:02 -0600

    add tensor packages

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -1,12 +1,20 @@
 import cv2
 import mediapipe as mp
 import time
+import tflite
+import xnnpack
+
+xnnpack_delegate = xnnpack.Delegate()
 
 cap = cv2.VideoCapture(0)
 
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+
 while True:
     success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result = hands.process(imgRGB)
 
     cv2.imshow("Image", img)
-    cv2.waitkey(0)
-
+    cv2.waitKey(0)
